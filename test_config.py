81606bc71d4dfcdf9a35b45996b3381f5320c3b6
Tanguy Lefort <tanguy_l@outlook.fr>
2022-05-18 17:09:29 +0200

    safe tests

--- test_config.py
+++ test_config.py
@@ -1,2 +1,12 @@
+import sys
+import pytest
+from benchopt.utils.sys_info import get_cuda_version
+
+
 def check_test_solver_install(solver_class):
-    pass
+    if "cuml" in solver_class.name.lower():
+        if sys.platform == "darwin":
+            pytest.xfail("Cuml is not supported on MacOS.")
+        cuda_version = get_cuda_version()
+        if cuda_version is None:
+            pytest.xfail("Cuml needs a working GPU hardware.")
