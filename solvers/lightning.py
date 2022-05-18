983d3475ba7ea8593aee3c0d9285749d988513db
Tanguy Lefort <tanguy_l@outlook.fr>
2022-05-18 17:00:10 +0200

    add solvers

---
+++ solvers/lightning.py
@@ -0,0 +1,51 @@
+from benchopt import BaseSolver, safe_import_context
+
+
+with safe_import_context() as import_ctx:
+    import numpy as np
+    from lightning.regression import CDRegressor
+
+
+# TODO: lightning always fit an intercept
+#       it is thus not optimizing the same cost function
+class Solver(BaseSolver):
+    name = 'Lightning'
+
+    install_cmd = 'conda'
+    requirements = [
+        'cython',
+        'pip:git+https://github.com/scikit-learn-contrib/lightning.git'
+    ]
+
+    references = [
+        'M. Blondel, K. Seki and K. Uehara, '
+        '"Block coordinate descent algorithms for large-scale sparse '
+        'multiclass classification" '
+        'Mach. Learn., vol. 93, no. 1, pp. 31-52 (2013)'
+    ]
+
+    def skip(self, X, y, lmbd, fit_intercept):
+        if fit_intercept:
+            return True, f"{self.name} does not handle fit_intercept"
+
+        return False, None
+
+    def set_objective(self, X, y, lmbd, fit_intercept):
+
+        self.X, self.y, self.lmbd = X, y, lmbd
+        self.fit_intercept = fit_intercept
+
+        self.ridge = CDRegressor(
+            loss='squared', penalty='l2', C=.6, alpha=self.lmbd,
+            tol=0, permute=False, shrinking=False, warm_start=False)
+
+    def run(self, n_iter):
+        self.ridge.max_iter = n_iter
+        self.ridge.fit(self.X, self.y)
+
+
+    def get_result(self):
+        beta = self.ridge.coef_.flatten()
+        if self.fit_intercept:
+            beta = np.r_[beta, self.ridge.intercept_]
+        return beta
---
+++ solvers/sklearn.py
@@ -0,0 +1,48 @@
+import warnings
+from benchopt import BaseSolver
+from benchopt import safe_import_context
+
+
+with safe_import_context() as import_ctx:
+    import numpy as np
+    from sklearn.linear_model import Ridge
+    from sklearn.exceptions import ConvergenceWarning
+
+
+class Solver(BaseSolver):
+    name = 'sklearn'
+
+    install_cmd = 'conda'
+    requirements = ['scikit-learn']
+
+    references = [
+        'F. Pedregosa, G. Varoquaux, A. Gramfort, V. Michel, B. Thirion, '
+        'O. Grisel, M. Blondel, P. Prettenhofer, R. Weiss, V. Dubourg, '
+        'J. Vanderplas, A. Passos, D. Cournapeau, M. Brucher, M. Perrot'
+        ' and E. Duchesnay'
+        '"Scikit-learn: Machine Learning in Python", J. Mach. Learn. Res., '
+        'vol. 12, pp. 2825-283 (2011)'
+    ]
+
+    def set_objective(self, X, y, lmbd, fit_intercept):
+        self.X, self.y, self.lmbd = X, y, lmbd
+        self.fit_intercept = fit_intercept
+
+        n_samples = self.X.shape[0]
+        self.ridge = Ridge(alpha=self.lmbd/n_samples,
+                           fit_intercept=fit_intercept, tol=0)
+        warnings.filterwarnings('ignore', category=ConvergenceWarning)
+
+    def run(self, n_iter):
+        if n_iter == 0:
+            self.coef = np.zeros([self.X.shape[1] + self.fit_intercept])
+        else:
+            self.ridge.max_iter = n_iter
+            self.ridge.fit(self.X, self.y)
+
+    def get_result(self):
+        coef = self.lasso.coef_.flatten()
+        if self.fit_intercept:
+            coef = np.r_[coef, self.lasso.intercept_]
+        self.coef = coef
+        return self.coef
