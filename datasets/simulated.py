a6597448304aec58f342917fe42351d703bdd911
Tanguy Lefort <tanguy_l@outlook.fr>
2022-05-19 17:38:50 +0200

    simulate as in lasso

--- datasets/simulated.py
+++ datasets/simulated.py
@@ -1,37 +1,30 @@
-from benchopt import BaseDataset, safe_import_context
-
-
-with safe_import_context() as import_ctx:
-    import numpy as np
-
-
 class Dataset(BaseDataset):
 
     name = "Simulated"
 
+    # List of parameters to generate the datasets. The benchmark will consider
+    # the cross product for each key in the dictionary.
     parameters = {
         'n_samples, n_features': [
-            (100, 50),
-            (1000, 2000)]
+            (100, 5_000),
+            (100, 10_000)
+        ],
+        'rho': [0, 0.6],
     }
 
-    def __init__(self, n_samples=10, n_features=50, random_state=27):
+    def __init__(self, n_samples=10, n_features=50, rho=0, random_state=27):
         # Store the parameters of the dataset
         self.n_samples = n_samples
         self.n_features = n_features
         self.random_state = random_state
+        self.rho = rho
 
     def get_data(self):
-
         rng = np.random.RandomState(self.random_state)
-        beta = rng.randn(self.n_features)
 
-        X = rng.randn(self.n_samples, self.n_features)
-        y = X @ beta
+        X, y, _ = make_correlated_data(self.n_samples, self.n_features,
+                                       rho=self.rho, random_state=rng)
 
-        X_test = rng.randn(self.n_samples, self.n_features)
-        y_test = X_test @ beta
-
-        data = dict(X=X, y=y, X_test=X_test, y_test=y_test)
+        data = dict(X=X, y=y)
 
         return data
