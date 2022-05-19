b1699506951d77f4502039540fd025b0ff5c85d2
Tanguy Lefort <tanguy_l@outlook.fr>
2022-05-19 16:16:12 +0200

    fix cd

--- solvers/cd.py
+++ solvers/cd.py
@@ -68,7 +68,7 @@ class Solver(BaseSolver):
                 if L[j] == 0.:
                     continue
                 old = w[j]
-                w[j] = w[j] + X[:, j] @ R / L[j] + lmbd * w[j]
+                w[j] = w[j] + (X[:, j] @ R + lmbd * w[j]) / L[j] 
                 diff = old - w[j]
                 if diff != 0:
                     R += diff * X[:, j]
