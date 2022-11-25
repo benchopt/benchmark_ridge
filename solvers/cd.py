45ee4b60c6202eb89825e279268f5fb7b5281e54
Badr MOUFAD <65614794+Badr-MOUFAD@users.noreply.github.com>
2022-11-25 20:11:46 +0000

    trailling space cd solver

--- solvers/cd.py
+++ solvers/cd.py
@@ -89,7 +89,7 @@ class Solver(BaseSolver):
                 scal = 0.
                 for ind in range(start, end):
                     scal += X_data[ind] * R[X_indices[ind]]
-                w[j] = w[j] + (scal  - lmbd * w[j]) / L[j]
+                w[j] = w[j] + (scal - lmbd * w[j]) / L[j]
                 diff = old - w[j]
                 if diff != 0:
                     for ind in range(start, end):
