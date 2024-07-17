c2414134b7a4621979ae15d854cbb13d2577434d
wassimmazouz <wassimmazouz5261@gmail.com>
2024-07-17 10:44:17 +0200

    install libomp directly

---
+++ benchmark_utils/__init__.py
@@ -0,0 +1,9 @@
+from libomp_install import install_libomp_on_macos
+
+# libomp x86_64 is needed for snapML on macOS. Homebrew is needed for this install as well.
+
+# Homebrew is installed and added to path to install libomp x86_64.
+
+# Also, libomp needs some directories added to PATH to function correctly.
+
+install_libomp_on_macos()
